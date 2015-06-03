6423b0a27fa01c4c7fc0b393b05697dbf6fcbd08
Bor Hodošček <bor.hodoscek@gmail.com>
2015-06-03 15:15:29 +0900

    updated deps

--- project.clj
+++ project.clj
@@ -3,10 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0-beta2"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
 
-                 [prismatic/schema "0.4.2"]
-                 [prismatic/plumbing "0.4.3"]
+                 [prismatic/schema "0.4.3"]
+                 [prismatic/plumbing "0.4.4"]
                  [me.raynes/fs "1.4.6"]
                  [org.apache.commons/commons-compress "1.9"]
                  [org.tukaani/xz "1.5"]
